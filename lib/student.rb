5a641d2e0c7f8032f7d55cf383e3ee4a0073b71f
Erick Castille <ecastille924@gmail.com>
2020-11-12 00:51:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,6 +37,8 @@ class Student
       SQL
       
       DB[:conn].execute(sql)
+      
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end
 
   # Remember, you can access your database connection anywhere in this class
