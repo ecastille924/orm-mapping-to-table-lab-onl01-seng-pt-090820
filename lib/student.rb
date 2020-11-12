a8a7f053d4c4aaae5cf3db29974b27d3c6a29718
Erick Castille <ecastille924@gmail.com>
2020-11-12 00:28:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
   
   def self.create_table
     sql = <<- SQL 
-    
+    CREATE TABLE IF NOT EXISTS 
   end
 
   # Remember, you can access your database connection anywhere in this class
