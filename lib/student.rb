b3945c2ce4697a7ef4be9414563c79dd50f15b97
Erick Castille <ecastille924@gmail.com>
2020-11-12 00:59:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,7 +41,8 @@ class Student
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
-  def self.create ( )
+  def self.create(name, grade)
+    
   end
 
   # Remember, you can access your database connection anywhere in this class
